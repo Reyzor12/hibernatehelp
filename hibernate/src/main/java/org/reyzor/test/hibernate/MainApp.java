aa38f894d2adbd06d53b1a68829b46dd6aaa4824
Reyzor12 <testaccwow001@gmail.com>
2017-09-01 13:55:54 +0300

    lesson 13 hql

--- hibernate/src/main/java/org/reyzor/test/hibernate/MainApp.java
+++ hibernate/src/main/java/org/reyzor/test/hibernate/MainApp.java
@@ -3,6 +3,7 @@ package org.reyzor.test.hibernate;
 import java.util.Iterator;
 import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -13,29 +14,22 @@ import org.reyzor.test.hibernate.util.HibernateUtil;
 public class MainApp {
 
 	public static void main(String[] args) {
-		
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		
 		Session session = sessionFactory.openSession();
-		//List<Object> products = null;
-		List<Object[]> products = null;
+		//List<Product> products = null;
+		List<Product> products = null;
+		
 		try {
 			session.beginTransaction();
-			//SQLQuery queryUpdate = session.createSQLQuery("UPDATE product SET title=:title WHERE id = :id");
-			SQLQuery queryUpdate = session.createSQLQuery("UPDATE product SET title=? WHERE id = ?");
-			//queryUpdate.setParameter("title", "123");
-			//queryUpdate.setParameter("id", 1);
-			queryUpdate.setParameter(0, "123");
-			queryUpdate.setParameter(1, 1);
-			queryUpdate.executeUpdate();
+		
+			//Query query = session.createQuery("FROM Product as p JOIN FETCH p.productCategory as pc");
+			//Query query = session.createQuery("SELECT p FROM Product as p INNER JOIN p.productCategory as pc WITH pc.id = ?");
+			//query.setParameter(0, 1L);
+			//products = query.list();
 			
-			//SQLQuery query = session.createSQLQuery("SELECT * FROM product");
-			SQLQuery querySelect = session.createSQLQuery("SELECT {p.*},{pc.*} FROM product p INNER JOIN product_category pc ON p.product_category_id=pc.id");
-			//query.addEntity(Product.class);
-			querySelect.addEntity("p", Product.class);
-			querySelect.addJoin("pc", "p.productCategory");
-			products = querySelect.list();
-			
+			Product product = new Product();
+			session.save(product);
 			
 			session.getTransaction().commit();
 		} catch(Exception e) {
@@ -46,13 +40,11 @@ public class MainApp {
 			sessionFactory.close();
 		}	
 		
-		for(Object[] obj: products) {
-			System.out.println(((Product) obj[0]).toString() + ((ProductCategory) obj[1]).getTitle() );
+		for(Product product:products) {
+			System.out.println(product.toString());
 		}
 		/*
-		for(Iterator iterator = products.iterator();iterator.hasNext(); ) {
-			
-			Product product = (Product)iterator.next();
+		for(Product product:products) {
 			System.out.println(product.toSrting());
 		}
 		*/
