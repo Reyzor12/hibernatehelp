06488679a9d89fd5fa4eacfad425ae77aedc70cc
Reyzor12 <testaccwow001@gmail.com>
2017-09-01 09:40:43 +0300

    lesson 11 transaction

--- hibernate/src/main/java/org/reyzor/test/hibernate/MainApp.java
+++ hibernate/src/main/java/org/reyzor/test/hibernate/MainApp.java
@@ -1,13 +1,28 @@
 package org.reyzor.test.hibernate;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.reyzor.test.hibernate.util.HibernateUtil;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-
+		
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		
+		Session session = sessionFactory.openSession();
+		
+		try {
+			session.beginTransaction();
+			
+			session.getTransaction().commit();
+		} catch(Exception e) {
+			session.getTransaction().rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+			sessionFactory.close();
+		}	
 		
 	}
 
